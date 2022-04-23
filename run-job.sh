ee80de19e6eb169716bb364b237f2d26fd044316
Oleksandr Minaiev <oleksminaiev@gmail.com>
2022-04-23 19:40:09 -0400

    upgrade to node16

--- run-job.sh
+++ run-job.sh
@@ -3,5 +3,4 @@ set -e
 
 # Generate API Spec
 node index.js api /var/apispec/data $PGHOST 5432 $PGDATABASE $PGUSER $PGPASSWORD 1
-node index.js esi /var/apispec/data $PGHOST 5432 $PGDATABASE $PGUSER $PGPASSWORD 1
 node index.js sde /var/apispec/data $PGHOST 5432 $PGDATABASE $PGUSER $PGPASSWORD 1
